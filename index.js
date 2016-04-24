35e46ec503f5b9c2fda258fad7a32868a0b1a141
ttymed <tyler.boright@gmail.com>
2016-04-24 23:52:59 +0800

    added commander and the start of options to the tool.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+#!/usr/bin/env node --harmony
+
+'use strict'
+let program = require('commander')
+
+program
+    .version('0.0.1')
+    .option('-h, --hundreds', 'convert to hundreds')
+    .option('-t, --thousands', 'convert to thousands')
+    .option('-m, --millions', 'convert to millions')
+    .option('-b, --billions', 'convert to millions')
+    .parse(process.argv)
+
+console.log('your number is ')
+if (program.hundreds)
+    if (program.thousands) console.log(' - ')
+    if (program.millions) console.log(' - ')
+    if (program.billions) console.log(' - ')
 
