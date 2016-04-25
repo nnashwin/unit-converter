06d5796107855f3ed232434600c9e51559e7e63b
ttymed <tyler.boright@gmail.com>
2016-04-25 13:09:19 +0800

    can now see the input number going into the function.  working on changing it based on parameters

--- index.js
+++ index.js
@@ -2,18 +2,32 @@
 
 'use strict'
 let program = require('commander')
+let inputNumber
 
 program
     .version('0.0.1')
+    .arguments('<cmd> [env]')
+    .action((cmd, env) => {
+        number = cmd
+        
+    })
     .option('-h, --hundreds', 'convert to hundreds')
     .option('-t, --thousands', 'convert to thousands')
     .option('-m, --millions', 'convert to millions')
     .option('-b, --billions', 'convert to millions')
-    .parse(process.argv)
 
-console.log('your number is ')
+
+program.parse(process.argv)
+
+if (typeof number === 'undefined') {
+    console.log('You need a number Bro!')
+    process.exit(1)
+} 
+
+
+console.log(`your number is ${number}`)
 if (program.hundreds)
-    if (program.thousands) console.log(' - ')
+    if (program.thousands) console.log(`${cmd}`)
     if (program.millions) console.log(' - ')
     if (program.billions) console.log(' - ')
 
